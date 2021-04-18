5c09798f88cf06f0149ae31240aec0addc772127
siemen <siemensubbaiah1@gmail.com>
2021-04-18 14:33:06 +0530

    added try-catch block

--- app.js
+++ app.js
@@ -8,35 +8,47 @@ let ham = document.querySelector('.ham');
 let flex = document.querySelector('.flex');
 ham.addEventListener('click', () => flex.classList.toggle('flex-show'));
 
-//API STUFF!
+//API!
 window.addEventListener('load', showNews);
 const apiURL = 'https://kodagu.today/newsarticles?_sort=published_at:desc';
 let cards = document.querySelector('.cards');
 
 async function showNews() {
-  const res = await fetch(apiURL);
-  const data = await res.json();
-  const ui = data
-    .map((news) => {
-      return `
-    <div data-aos="fade-up" class="card">
-        <img src=${news.image.url} alt="">
-            <div class="content">
-                <h2 class = "title">${news.title}</h2>
-                <p class = "category">Category : ${news.newsCategories}</p>
-                </a>
-                <div class="details">
-                    <p>${new Date(news.published_at).toLocaleDateString()}</p>
-                    <a class="btn" href="https://play.google.com/store/apps/details?id=com.kodagu.now">Read more</a>
-                </div>
-            </div>
-    </div>
+  try {
+    const res = await fetch(apiURL);
+    const data = await res.json();
+    const ui = data
+      .map((news) => {
+        return `
+      <div data-aos="fade-up" class="card">
+          <img src=${news.image.url} alt="">
+              <div class="content">
+                  <h2 class = "title">${news.title}</h2>
+                  <p class = "category">Category : ${news.newsCategories}</p>
+                  <div class="details">
+                      <p>${new Date(news.published_at).toLocaleDateString()}</p>
+                      <a class="btn" href="https://play.google.com/store/apps/details?id=com.kodagu.now">Read more</a>
+                  </div>
+              </div>
+      </div>
+      `;
+      })
+      .join('');
+    cards.innerHTML = ui;
+  } catch (error) {
+    const errorMsg = `
+     <div data-aos="fade-up" class="card">
+              <div class="content">
+                  <img class="server-down" src="/images/server-down.svg"/>
+                  <h1 class = "title" style="text-align:center;margin:1rem 0">Cannot Fetch news due to network error</h1>
+              </div>
+      </div>
     `;
-    })
-    .join('');
-  cards.innerHTML = ui;
+    cards.innerHTML = errorMsg;
+  }
 }
 
+//AOS!
 AOS.init();
 
 //SEARCH
